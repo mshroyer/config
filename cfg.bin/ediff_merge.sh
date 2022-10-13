4b2e5612772d222c7a64c015e7a938897e63f8e6
Mark Shroyer <mark@shroyer.name>
2022-10-13 13:53:05 -0700

    Don't use emacsclient for ediff merge

--- cfg.bin/ediff_merge.sh
+++ cfg.bin/ediff_merge.sh
@@ -7,5 +7,5 @@ in_other="$(realpath $2)"
 in_base="$(realpath $3)"
 out="$(realpath $4)"
 
-emacsclient -c -nw --alternate-editor=emacs -q --eval \
+emacs -nw -q --eval \
 	    "(ediff-merge-with-ancestor \""$in_local"\" \""$in_other"\" \""$in_base"\" nil \""$out"\")"
