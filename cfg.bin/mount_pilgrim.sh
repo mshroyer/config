e76b68bf23747b9a8267760610e1229e16ec4dca
Mark Shroyer <mark@shroyer.name>
2025-01-25 10:01:51 -0800

    [pilgrim] Support macOS in the mount script

--- cfg.bin/mount_pilgrim.sh
+++ cfg.bin/mount_pilgrim.sh
@@ -1,15 +1,32 @@
 #!/bin/sh
 
-# Mounts my home directory on pilgrim, on Linux.  May require cifs-utils to be
-# installed first.
+# Mounts my home directory on pilgrim.  On Linux, this may require cifs-utils
+# to be installed first.
 
 set -e
 
-local mountpoint="$HOME/h"
+function mount_linux() {
+	local mountpoint="$HOME/h"
 
-if [ ! -d "$mountpoint" ]; then
-	mkdir "$mountpoint"
+	if [ ! -d "$mountpoint" ]; then
+		mkdir "$mountpoint"
+	fi
+
+	sudo mount -t cifs -o username=$(whoami),uid=$(id -u),gid=$(id -g) \
+	     //pilgrim.homestarmy.net/$(whoami) "$mountpoint"
+}
+
+function mount_macos() {
+	# Take advantage of Keychain password caching.
+	echo "mount volume \"smb://pilgrim.homestarmy.net/$(whoami)\"" \
+		| /usr/bin/osascript
+}
+
+if [ "$(uname)" = "Linux" ]; then
+	mount_linux
+elif [ "$(uname)" = "Darwin" ]; then
+	mount_macos
+else
+	echo "Unsupported on $(uname)" >&1
+	exit 1
 fi
-
-sudo mount -t cifs -o username=$(whoami),uid=$(id -u),gid=$(id -g) \
-      //pilgrim.homestarmy.net/$(whoami) "$mountpoint"
